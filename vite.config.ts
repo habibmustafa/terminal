426d203bb96919f09bd5809c26e85ae50b7c3666
Habibullah Mustafazadeh <hmustafazadeh@rth.gov.az>
2025-12-16 11:31:04 +0400

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { splitVendorChunkPlugin, defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "@svgr/rollup";
 
@@ -7,28 +7,28 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
   },
-  build: {
-    outDir: "dist",
-    chunkSizeWarningLimit: 2000,
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "chunks/[name][hash].js",
-        assetFileNames: "assets/[name][hash].[ext]",
-        manualChunks: (path) =>
-          path.split("/").reverse()[
-            path.split("/").reverse().indexOf("node_modules") - 1
-          ],
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      fsevents$: "fsevents.js",
-      src: "/src",
-    },
-  },
-  plugins: [svgr(), react(), splitVendorChunkPlugin()],
+  // build: {
+  //   outDir: "dist",
+  //   chunkSizeWarningLimit: 2000,
+  //   emptyOutDir: true,
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: "[name].js",
+  //       chunkFileNames: "chunks/[name][hash].js",
+  //       assetFileNames: "assets/[name][hash].[ext]",
+  //       manualChunks: (path) =>
+  //         path.split("/").reverse()[
+  //           path.split("/").reverse().indexOf("node_modules") - 1
+  //         ],
+  //     },
+  //   },
+  // },
+  // resolve: {
+  //   alias: {
+  //     fsevents$: "fsevents.js",
+  //     src: "/src",
+  //   },
+  // },
+  plugins: [svgr(), react()],
   base: "/",
 });

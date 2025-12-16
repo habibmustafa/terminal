e7e0531a65280b979f3535c53a7216c0537ff0c5
Habibullah Mustafazadeh <hmustafazadeh@rth.gov.az>
2025-12-16 11:43:22 +0400

    change

--- vite.config.ts
+++ vite.config.ts
@@ -7,28 +7,28 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
   },
-  // build: {
-  //   outDir: "dist",
-  //   chunkSizeWarningLimit: 2000,
-  //   emptyOutDir: true,
-  //   rollupOptions: {
-  //     output: {
-  //       entryFileNames: "[name].js",
-  //       chunkFileNames: "chunks/[name][hash].js",
-  //       assetFileNames: "assets/[name][hash].[ext]",
-  //       manualChunks: (path) =>
-  //         path.split("/").reverse()[
-  //           path.split("/").reverse().indexOf("node_modules") - 1
-  //         ],
-  //     },
-  //   },
-  // },
-  // resolve: {
-  //   alias: {
-  //     fsevents$: "fsevents.js",
-  //     src: "/src",
-  //   },
-  // },
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 2000,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "chunks/[name][hash].js",
+        assetFileNames: "assets/[name][hash].[ext]",
+        manualChunks: (path) =>
+          path.split("/").reverse()[
+            path.split("/").reverse().indexOf("node_modules") - 1
+          ],
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      fsevents$: "fsevents.js",
+      src: "/src",
+    },
+  },
   plugins: [svgr(), react()],
   base: "/",
 });
